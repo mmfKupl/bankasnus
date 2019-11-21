3483d22610d355344219f1c930f49d091ae2cdcf
Ivan Kuplevich <ikuplevich97@gmail.com>
2019-11-21 13:27:56 +0300

    add page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,11 @@ import { TransferHttpCacheModule } from '@nguniversal/common';
 import { ContactFormComponent } from './contact-form/contact-form.component';
 import { SliderComponent } from './slider/slider.component';
 import { ImageSetComponent } from './image-set/image-set.component';
+import { HeaderInfoComponent } from './header-info/header-info.component';
+import { PageComponent } from './page/page.component';
 
 @NgModule({
-  declarations: [AppComponent, ContactFormComponent, SliderComponent, ImageSetComponent],
+  declarations: [AppComponent, ContactFormComponent, SliderComponent, ImageSetComponent, HeaderInfoComponent, PageComponent],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
