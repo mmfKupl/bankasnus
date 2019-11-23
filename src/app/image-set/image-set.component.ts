265e5c16c54e9aa6af4663359dcf6d6c195f28d2
RomanChirkov <chirk0ff.poker@gmail.com>
2019-11-23 18:24:50 +0300

    gallery-page

--- src/app/image-set/image-set.component.ts
+++ src/app/image-set/image-set.component.ts
@@ -6,8 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./image-set.component.scss']
 })
 export class ImageSetComponent implements OnInit {
-
-  constructor() { }
+  img: Array<string>;
+  constructor() {
+    this.img = [
+      '../../assets/1.jpg',
+      '../../assets/2.jpg',
+      '../../assets/3.jpg',
+      '../../assets/4.jpg',
+      '../../assets/5.jpg',
+      '../../assets/6.jpg',
+      '../../assets/7.jpg',
+      '../../assets/8.jpg',
+      '../../assets/9.jpg',
+      '../../assets/10.jpg',
+      '../../assets/11.jpg',
+      '../../assets/12.jpg'];
+  }
 
   ngOnInit() {
   }
